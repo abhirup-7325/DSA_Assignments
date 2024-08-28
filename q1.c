e24396ee714c205f975503cedb389914524fed47
abhirup-7325 <palabhirup07032005@gmail.com>
2024-08-28 17:22:37 +0530

    Program 1 completed and tested

--- q1.c
+++ q1.c
@@ -10,7 +10,7 @@ there is overflow in the result and change the data types for accommodating high
 
 
 void getIO(int *);
-void factorialRecursive(int);
+void factorialRecursive(int, int, bool, int, long long);
 void factorialIterative(int);
 
 
@@ -18,8 +18,8 @@ int main() {
     int n;
     getIO(&n);
 
-    factorialRecursive(n);
     factorialIterative(n);
+    factorialRecursive(n, 1, false, 1, 1);
 }
 
 
@@ -57,8 +57,38 @@ void factorialIterative(int n) {
 }
 
 
-void factorialRecursive(int n) {
-    
+void factorialRecursive(int n, int i, bool isLong, int intFact, long long longFact) {
+    if (i == n + 1) {
+        char text[100] = "The factorial using iterative method is:";
+
+        if (isLong) {
+            printf("%s %lld\n", text, longFact); 
+        } else {
+            printf("%s %d\n", text, intFact);
+        }
+
+        return;
+    }
+
+
+    if (isLong) {
+        if ((LONG_MAX / longFact) < i) {
+            printf("Factorial is too large to be represented. Aborting...\n");
+            exit(0);
+        } else {
+            longFact *= i;
+        }        
+    } else {
+        if ((INT_MAX / intFact) >= i) {
+                intFact *= i;
+            } else {
+                longFact = intFact;
+                longFact *= i;
+                isLong = true;
+            }
+    }
+
+    factorialRecursive(n, i + 1, isLong, intFact, longFact);
 }
 
 
